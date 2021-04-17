120cf9f8fba2abd968294af6dcf7bed6131f35ec
Dhruv Shetty <dhruvshetty3@gmail.com>
2021-04-17 16:05:13 +0530

    removing random dependency on lineItems

--- django_project/ecommerce_app/models.py
+++ django_project/ecommerce_app/models.py
@@ -44,7 +44,4 @@ class Order(models.Model):
     def __str__(self):
         return "{}:{}".format(self.id, self.email)
 
-    def total_cost(self):
-        return sum([ li.cost() for li in self.lineitem_set.all() ] )
-
 
